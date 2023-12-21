24e4b0598e3b1241818d65fe8c552c76553eccf2
as6620 <as6620@bs.amalnet.k12.il>
2023-12-21 15:58:39 +0200

    second

--- app/src/main/java/com/example/task54/MainActivity.java
+++ app/src/main/java/com/example/task54/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.task54;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView tv;
+    int counterClick = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tv = findViewById(R.id.tv);
     }
 }
